52e7c296267051d263f206b853e7b916539fe3ae
jeremycote <jeremymcote@gmail.com>
2023-08-10 15:20:42 -0400

    Simulate lap time, update dashboard

--- main/src/main.c
+++ main/src/main.c
@@ -81,11 +81,17 @@ int main(int argc, char **argv)
   DataAggregatorWrapper* wrapper = DataAggregator_Create();
   Application_Create(wrapper);
 
+  uint32_t time = 0;
+
   while(1) {
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
     lv_timer_handler();
     usleep(5 * 1000);
+
+    time += 5;
+
+    SetLapTime(wrapper, time);
   }
 
   return 0;
