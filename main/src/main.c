cb4c97d9521964ea0ec766466396a68e1e65a736
jeremycote <jeremymcote@gmail.com>
2023-08-18 15:27:21 -0400

    Update core

--- main/src/main.c
+++ main/src/main.c
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
   hal_init();
 
-  DataAggregatorWrapper* wrapper = DataAggregator_Create();
+  DataAggregatorWrapper* wrapper = DataAggregator_Create(10);
   Application_Create(wrapper);
 
   uint32_t time = 0;
